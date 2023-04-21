66b87c9b3c532461bcc192b0cfad806882ac8a27
aledguedes <alexandredguedes@gmail.com>
2023-04-21 20:23:27 -0300

    env upload

--- src/main/java/com/reccos/admin/utils/UploadService.java
+++ src/main/java/com/reccos/admin/utils/UploadService.java
@@ -17,7 +17,7 @@ public class UploadService {
 		System.out.println("UPLOAD IMAGEM: "+file);
 		
 		try {
-			String caminho = "D:\\TCC\\imagens\\"+slug;
+			String caminho = "D:\\TCC\\FRONT\\admin\\src\\assets\\"+slug;
 			File dir = new File(caminho);
 			if (!dir.exists()) {
 				dir.mkdirs();
